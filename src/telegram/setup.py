c12a8d17bb5269e11ffb4ae4f8500f82237143ee
David Blanco <david@webtools.es>
2023-01-01 05:43:58 +0100

    refactor

--- src/telegram/setup.py
+++ src/telegram/setup.py
@@ -4,11 +4,13 @@ import logging
 from aiogram import Bot, Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from src.utils.type_helpers import Config
+
 config = Config()
-
 from src.utils.framex_utils import FrameXBisector
-bisector = FrameXBisector()
 
+logging.basicConfig(level=logging.INFO)
+
+bisector = FrameXBisector()
 bot = Bot(token=config.telegram_token)
 storage = MemoryStorage()
 dp = Dispatcher(bot, storage=storage)
